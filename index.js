bb3962693242cf250623234283e0eeee1e88ebd3
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 16:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
     if(katzDeliLine === 0){
       return "There is nobody waiting to be served!"
     } else {
-      return 'Currently serving ${katzDeliLine.shift()}'
+      return 'Currently serving ${katzDeliLine.shift()}';
     }
   }
 }
