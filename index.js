0df842f4c88588745a8736027029d3e92d1130f2
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 16:40:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli} in line.`
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 function nowServing(katzDeliLine) {
   return katzDeliLineli[0].unshift(0);
