44712c03d1a8153c13fe46aa48aa41591ac997cb
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 17:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(katzDeliLine) {
- 
   for(let i = 0; i < katzDeliLine.length; i++){
-    
     if(katzDeliLine.length === 0){
       return "There is nobody waiting to be served!"
     } else {
-      return `Currently serving + ${katzDeliLine.shift()}.`;
+      return `Currently serving ${katzDeliLine.shift()}.`;
     }
   }
 }
