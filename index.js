5fb8ea73e8e6413d84ab5fabc0164993e119578f
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 17:36:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ function nowServing(katzDeliLine) {
   }
 }
 function currentLine(katzDeliLine) {
-  let str = '';
+  // let str = '';
   if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }
-  let arr = 
+  let arr = [];
+  
 }
 
 // {
