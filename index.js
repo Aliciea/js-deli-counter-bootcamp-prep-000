e606bef34ef0bc6009f70e0129fc1b28662b56f2
Aliciea D Suechin <adr330@nyu.edu>
2019-06-05 17:05:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   let arr = [];
   for(let i = 0; i < katzDeliLine.length; i++){
-        arr.push(` `+[i+1]+`. `  + katzDeli[i]);
+        arr.push(` `+[i+1]+`. `  + katzDeliLine[i]);
     if(katzDeliLine.length === 0){
       return "The line is currently empty."
     }
